1e9fc3e3baea2d33ef4eb321ee1d93964a366221
Priya Chowdhury <78656082+Priya077@users.noreply.github.com>
2026-08-25 19:09:57 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var res = new Dictionary<string, List<string>>();
+        foreach(var s in strs)
+        {
+            char[] temp=s.ToCharArray();
+            Array.Sort(temp);
+            string sortedS=new String(temp);
+            if(!res.ContainsKey(sortedS))
+            {
+                res[sortedS]= new List<string>();
+            }
+            res[sortedS].Add(s);
+        }
+        return res.Values.ToList<List<string>>();
+    }
+}
