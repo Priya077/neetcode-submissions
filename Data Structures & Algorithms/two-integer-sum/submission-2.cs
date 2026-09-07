053902019b7fda6deadbdd2186df65777af45811
Priya Chowdhury <78656082+Priya077@users.noreply.github.com>
2026-09-07 12:45:02 +0530

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int,int>map=new Dictionary<int,int>();
+        for(int i=0;i<nums.Length;i++)
+        {
+            int temp=nums[i];
+            int rem=target-nums[i];
+            if(map.ContainsKey(rem))
+            {
+                return new int[]{map[rem],i};
+            }
+            map[nums[i]]=i;
+        }
+        return new int[0];
+    }
+}
